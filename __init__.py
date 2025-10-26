b58d6185a86701b1c36853ea9aefe0513432bde5
Dcnigma <bart_is@hotmail.com>
2025-10-26 14:06:23 +0100

    Set window to stay on top and activate it
    Ensure the window stays on top and is activated.

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@
 # ui_layout.addItem(QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
 ui_layout.addStretch(1)
 self.apps = apps  # in __init__
+self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+self.show()
+self.raise_()
+self.activateWindow()
