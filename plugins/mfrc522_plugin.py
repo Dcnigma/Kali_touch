cfc9e1dea06552d03294474648e24713a60fc726
Dcnigma <bart_is@hotmail.com>
2025-10-28 01:24:04 +0100

    Update mfrc522_plugin.py

--- plugins/mfrc522_plugin.py
+++ plugins/mfrc522_plugin.py
@@ -33,8 +33,8 @@ class MFRC522Plugin(QWidget):
         super().__init__(parent)
         self.cfg = cfg
         self.setWindowTitle("RFID Reader")
-        self.setFixedSize(1200, 900)
-        self.setMinimumSize(1200, 900)
+        self.setFixedSize(1150, 900)
+        self.setMinimumSize(1150, 900)
         self.cards = self.load_cards()  # Load stored UIDs
         self.page = 0
         self.checkboxes = []
