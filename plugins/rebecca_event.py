5c924f4d03bf5b93fe42fcc908c8f1939a7f937f
Dcnigma <bart_is@hotmail.com>
2025-10-29 23:09:42 +0100

    Update rebecca_event.py

--- plugins/rebecca_event.py
+++ plugins/rebecca_event.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys, socket, json
-if len(sys.argv)<2:
+if len(sys.argv) < 2:
     print("usage: rebecca_event.py <event>")
     exit()
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-sock.sendto(json.dumps({"type":sys.argv[1]}).encode(), "/tmp/rebecca.sock")
+sock.sendto(json.dumps({"type": sys.argv[1]}).encode(), "/tmp/rebecca.sock")
 sock.close()
