49124756c68d8f39a1a92cca4215d0269ec0a15f
Dcnigma <bart_is@hotmail.com>
2025-10-29 19:05:43 +0100

    Update gpio_keyboard.py

--- gpio_keyboard.py
+++ gpio_keyboard.py
@@ -1,38 +1,26 @@
-#!/usr/bin/env python3
+import uinput
 import RPi.GPIO as GPIO
-from pynput.keyboard import Controller, Key
 import time
 
-# Setup
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-# Button pin configuration
 buttons = {
-    19: Key.right,
-    13: Key.up,
-    6: Key.down,
-    5: Key.left,
-    26: Key.esc  # Home button mapped to ESC
+    19: uinput.KEY_RIGHT,
+    13: uinput.KEY_UP,
+    6: uinput.KEY_DOWN,
+    5: uinput.KEY_LEFT,
+    26: uinput.KEY_ESC
 }
 
-for pin in buttons:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+device = uinput.Device(buttons)
 
-keyboard = Controller()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(list(buttons.keys()), GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 last_state = {pin: GPIO.input(pin) for pin in buttons}
 
-try:
-    while True:
-        for pin, key in buttons.items():
-            current_state = GPIO.input(pin)
-            if last_state[pin] == GPIO.HIGH and current_state == GPIO.LOW:
-                # Button just pressed
-                keyboard.press(key)
-                keyboard.release(key)
-            last_state[pin] = current_state
-        time.sleep(0.01)  # 10ms debounce
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    GPIO.cleanup()
+while True:
+    for pin, key in buttons.items():
+        current_state = GPIO.input(pin)
+        if last_state[pin] == GPIO.HIGH and current_state == GPIO.LOW:
+            device.emit_click(key)
+        last_state[pin] = current_state
+    time.sleep(0.01)
