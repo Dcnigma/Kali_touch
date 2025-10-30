8128bb2ef0e01ce08e6adc846d52a86db9d3de4b
Dcnigma <bart_is@hotmail.com>
2025-10-30 18:44:01 +0100

    Update Passport.py

--- plugins/Passport.py
+++ plugins/Passport.py
@@ -17,15 +17,15 @@ REBECCA_XP_JSON = os.path.join(plugin_folder, "rebecca_xp.json")
 FACES_DIR = os.path.join(plugin_folder, "oLed", "rebecca", "faces_rebecca")
 
 # Photo frame positions & size
-FRAME_X, FRAME_Y = 67, 70
-FRAME_W, FRAME_H = 360, 360
+FRAME_X, FRAME_Y = 75, 70
+FRAME_W, FRAME_H = 350, 350
 
 # Other positions
 NAME_Y = 97
 MOOD_Y = 222
 LEVEL_Y = 348
 PROGRESS_X, PROGRESS_Y = 473, 410
-PROGRESS_W, PROGRESS_H = 495, 65
+PROGRESS_W, PROGRESS_H = 495, 66
 
 LEVELS = [0, 50, 150, 350, 700, 1200]
 
