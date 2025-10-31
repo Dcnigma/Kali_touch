e91e83d4f728e6073053ffbbf0250969d84c5e0f
Dcnigma <bart_is@hotmail.com>
2025-10-31 13:47:15 +0100

    Update Passport.py

--- plugins/Passport.py
+++ plugins/Passport.py
@@ -27,7 +27,7 @@ class PassportPlugin(QWidget):
         self.apps = apps
         self.cfg = cfg
 
-        self.setFixedSize(1015, 590)
+        self.setFixedSize(1015, 600)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
         self.showFullScreen()
 
